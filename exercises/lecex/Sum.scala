2ac87a8f34ce3b1c6bd66cd976191f661bae4663
fabianfroding <fabian.froding@gmail.com>
2020-09-02 11:42:49 +0200

    finish sum function

--- exercises/lecex/Sum.scala
+++ exercises/lecex/Sum.scala
@@ -1,18 +1,15 @@
 object Sum {
-    def sum(i: Int, n: Int): Int = {
-        if (n <= 0) {
-            0
-        }
-        else if (n == 1) {
-            1
-        } else {
-            sum(n - 1) + sum(n - 2)
-        }
+    def sum(n: Int): Int = {
+        def go(n: Int, res: Int): Int =
+            if (n <= 0) res
+            else go(n - 1, n + res)
+        
+        go(n, 0)
     }
 
     def formatSum(n: Int) = {
         val msg = "The sum of all numbers <= %d is %d"
-        msg.format(n, sum(0, n))
+        msg.format(n, sum(n))
     }
 
     def main(args: Array[String]): Unit = {
