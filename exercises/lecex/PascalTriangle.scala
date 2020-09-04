00005a5f8b33b453efcb764863410292c5e96a79
fabianfroding <fabian.froding@gmail.com>
2020-09-04 18:54:41 +0200

    start work on pascal algo

---
+++ exercises/lecex/PascalTriangle.scala
@@ -0,0 +1,14 @@
+object PascalTriangle {
+    def pascal(n: Int, k: Int): Int = {
+
+    }
+
+    def formatPascal(n: Int) = {
+        val msg = "hey %d %d"
+        msg.format(n, pascal(0, n))
+    }
+
+    def main(args: Array[String]): Unit = {
+        println("wesh")
+    }
+}
