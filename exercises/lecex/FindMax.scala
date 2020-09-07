1dbec57d3f62f673091f9a61538bda690814bd5b
fabianfroding <fabian.froding@gmail.com>
2020-09-07 18:12:51 +0200

    start work find max in list

---
+++ exercises/lecex/FindMax.scala
@@ -0,0 +1,21 @@
+object FindMax {
+    def max(l: List[Int]): Int = {
+        def go(l: List[Int], m: Int): Int = {
+            if (m < l.tail(0)) go(l.tail, l.tail(0))
+            else 
+        }
+
+        go(l, l.head)
+    }
+
+    def formatMax(l: List[Int]) = {
+        val msg = "The max value in %s is %d"
+        msg.format(l, max(l))
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(formatMax(List(1, 6, 3, 7, 2))) // = 7
+        println(formatMax(List(-1, 56, 34, -43, 4, 110))) // = 110
+        println(formatMax(List(5))) // = 5
+    }
+}
