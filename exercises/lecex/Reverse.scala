61fd8ed20ec456228d62fc86ae49a6f03da0524a
fabianfroding <fabian.froding@gmail.com>
2020-09-08 12:36:06 +0200

    impl reverse list but needs clarification

---
+++ exercises/lecex/Reverse.scala
@@ -0,0 +1,21 @@
+object Reverse {
+    def reverse(l: List[Int]): List[Int] = {
+        def go(l: List[Int], res: List[Int]): List[Int] = {
+            if (l.isEmpty) res
+            else go(l.tail, l.head :: res) // Why does it APPEND the element? The :: is suposed to PREPEND.
+        }
+
+        go(l, List[Int]())
+    }
+
+    def formatReverse(l: List[Int]) = {
+        val msg = "%s reversed is %s"
+        msg.format(l, reverse(l))
+    }
+
+    def main(args: Array[String]): Unit = {
+        println(formatReverse(List(1, 2, 3, 4))) // = List(4, 3, 2, 1)
+        println(formatReverse(List(0, 34, -43, 4, 110))) // = List(110, 4, -43, 34, 0)
+        println(formatReverse(List(5))) // = List(5)
+    }
+}
