7c1f328abbfce14794b49f0065f97c5886eee5c5
fabianfroding <fabian.froding@gmail.com>
2020-09-13 10:55:09 +0200

    work on kth smallest element

--- exercises/lecex/NthSmallNum.scala
+++ exercises/lecex/NthSmallNum.scala
@@ -1,14 +1,29 @@
 object NthSmallNum {
     def nSmallest(l: List[Int], n: Int): Int = {
-        def go(l: List[Int], n: Int, smallest: Int): Int = {
-            if (l.tail.isEmpty) smallest
-            else if (l.head < smallest) go(l.tail, n, l.head)
-            else go(l.tail, n, smallest)
+        // Algo: sort the list, then return he k-1th element
+
+        def go(l: List[Int], n: Int, smallest: Int, orgL: List[Int]): Int = {
+            if (n > 0 && !l.tail.isEmpty) {
+                if (l.head < smallest) go(l.tail, n-1, l.head, l)
+                else go(l.tail, n, smallest, l)
+            }
+            else {
+                smallest
+            }
+            
         }
 
         if (l.isEmpty) 0
         else if (l.tail.isEmpty) l.head
-        else go(l.tail, n, l.head)
+        else go(l.tail, n, l.head, l)
+    }
+
+    def sort(l: List[Int]): List[Int] = {
+        def go(l: List[Int], newL: List[Int]): List[Int] = {
+            
+        }
+
+        go(l, List[Int]())
     }
 
     def formatNSmallest(l: List[Int], n: Int) = {
